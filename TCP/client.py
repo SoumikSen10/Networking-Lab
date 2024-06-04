9a01cde271f8bc12dd8ddb8d8c58257b4c9f172a
Soumik Sen <mastersoumik2003@gmail.com>
2024-06-04 13:01:29 +0530

    Applied final changes

--- TCP/client.py
+++ TCP/client.py
@@ -3,20 +3,19 @@ import socket
 server_ip = '127.0.0.1'
 server_port = 8002
 
-s = socket.socket() #create client side socket
+s = socket.socket()
+s.connect((server_ip, server_port))
 
-s.connect((server_ip,server_port))
+print("Available operations: addition, subtraction, multiplication, division, fibonacci, primecheck , leap_yearcheck")
 
-data_to_server = input('Operation number1 number2: ')  #enter client's message to server
+data_to_server = input('Operation number1 number2: ')
 
+while data_to_server != 'exit':
+    s.send(data_to_server.encode())
 
-while data_to_server != 'exit': #keep on sending data to server until 'exit' is entered
-    s.send(data_to_server.encode()) #send byte data to server
+    data_from_server = s.recv(1024)
+    print(data_to_server.split()[0] + ' result: ' + str(data_from_server.decode()))
 
-    data_from_server = s.recv(1024) #receive data from server
-    print(data_to_server.split()[0]+' is ' + str(data_from_server.decode())) #display server's message in string format
+    data_to_server = input('Operation number1 number2: ')
 
-    data_to_server = input('Operation number1 number2: ') #enter data to be sent to server
-
-s.close() #close connection
-    
+s.close()
--- TCP/server.py
+++ TCP/server.py
@@ -1,49 +1,90 @@
 import socket
 
-def addition(a,b):
-    return str(a+b)
+def addition(a, b):
+    return str(a + b)
 
-def subtraction(a,b):
-    return str(a-b)
+def subtraction(a, b):
+    return str(a - b)
 
-def multiplication(a,b):
-    return str(a*b)
+def multiplication(a, b):
+    return str(a * b)
 
-def division(a,b):
-    return str(a/b)
+def division(a, b):
+    if b == 0:  # Handling division by zero
+        return "Division by zero error"
+    return str(a / b)
+
+def fibonacci(n):
+    a, b = 0, 1
+    fib_sequence = [a, b]
+    for _ in range(n - 2):
+        a, b = b, a + b
+        fib_sequence.append(b)
+    return str(fib_sequence)
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+def check_prime(n):
+    if is_prime(n):
+        return "Prime"
+    else:
+        return "Not Prime"
+
+def is_leap_year(year):
+    if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+        return True
+    else:
+        return False
+
+def check_leap_year(year):
+    if is_leap_year(year):
+        return "Leap Year"
+    else:
+        return "Not a Leap Year"
 
 host = '127.0.0.1'
 port = 8002
 
-s = socket.socket() #create server side socket
-
-s.bind((host,port)) #binding the socket to host and port
-
-s.listen(1) # max num of connections allowed
-
-c, addr = s.accept() #wait till a client connects
-
+s = socket.socket()
+s.bind((host, port))
+s.listen(1)
+c, addr = s.accept()
 print('A client connected')
 
-#server runs continuously until all data is received from client
 while True:
-    data_received = c.recv(1024) #receive byte data from client
-
-    if not data_received: #if client sends empty string, stop
+    data_received = c.recv(1024)
+    if not data_received:
         break
-    
-    print('Data from client: ' + str(data_received.decode())) #display clients raw/byte message as string
-    data = data_received.decode().split()
-    n1, n2 = map(int, data[1:])
-    data_to_send = ''
-    if data[0] == 'addition':
-        data_to_send = addition(n1,n2) #server's message to client
-    elif data[0] == 'subtraction':
-        data_to_send = subtraction(n1,n2)
-    elif data[0] == 'multiplication':
-        data_to_send = multiplication(n1,n2)
-    elif data[0] == 'division':
-        data_to_send = division(n1,n2)
 
-    c.send(data_to_send.encode()) #send the data to client in byte format
-c.close() #close connection
+    data_str = data_received.decode()
+    print('Data from client: ' + data_str)
+
+    data = data_str.split()
+    
+    operation = data[0]
+    if operation == 'addition':
+        result = addition(int(data[1]), int(data[2]))
+    elif operation == 'subtraction':
+        result = subtraction(int(data[1]), int(data[2]))
+    elif operation == 'multiplication':
+        result = multiplication(int(data[1]), int(data[2]))
+    elif operation == 'division':
+        result = division(int(data[1]), int(data[2]))
+    elif operation == 'fibonacci':
+        result = fibonacci(int(data[1]))
+    elif operation == 'is_prime':
+        result = check_prime(int(data[1]))
+    elif operation == 'is_leap_year':
+        result = check_leap_year(int(data[1]))
+    else:
+        result = 'Invalid operation'
+
+    c.send(result.encode())
+
+c.close()
